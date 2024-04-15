849de206701019b5d21d3103f6fa72f814d2a439
Roman <rkennke@amazon.de>
2024-04-15 05:49:48 +0000

    Some fixes

--- src/hotspot/share/runtime/arguments.cpp
+++ src/hotspot/share/runtime/arguments.cpp
@@ -2968,7 +2968,7 @@ jint Arguments::finalize_vm_init_args(bool patch_mod_javabase) {
     warning("Compact object headers require compressed class pointers. Disabling compact object headers.");
     FLAG_SET_DEFAULT(UseCompactObjectHeaders, false);
   }
-  if (UseCompactObjectHeaders && LockingMode == LM_LEGACY) {
+  if (UseCompactObjectHeaders && LockingMode != LM_LIGHTWEIGHT) {
     FLAG_SET_DEFAULT(LockingMode, LM_LIGHTWEIGHT);
   }
   if (UseCompactObjectHeaders && !UseCompressedClassPointers) {
